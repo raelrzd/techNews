6a07c8d55c17b2d5193474c925a03139a60c2e3b
israel <israelrezendeluiz2903@gmail.com>
2022-10-31 09:11:30 -0300

    Criada viewModel com dependencia e conexão com o reporitório, para isso foi modificado o factory do provider interno da viewModel.

--- app/src/main/java/br/com/alura/technews/ui/viewmodel/ListaNoticiasViewModel.kt
+++ app/src/main/java/br/com/alura/technews/ui/viewmodel/ListaNoticiasViewModel.kt
@@ -2,16 +2,22 @@ package br.com.alura.technews.ui.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import br.com.alura.technews.model.Noticia
+import br.com.alura.technews.repository.NoticiaRepository
 
-class ListaNoticiasViewModel : ViewModel() {
+class ListaNoticiasViewModel (private val repository: NoticiaRepository) : ViewModel() {
 
-    init {
-        Log.i("viewmodel", "criando viewModel ")
-    }
+//    init {
+//        Log.i("viewmodel", "criando viewModel ")
+//    }
+//
+//    override fun onCleared() {
+//        super.onCleared()
+//        Log.i("viewmodel", "destruindo viewModel")
+//    }
 
-    override fun onCleared() {
-        super.onCleared()
-        Log.i("viewmodel", "destruindo viewModel")
+    fun buscaTodos(quandoSucesso: (noticiasNovas: List<Noticia>) -> Unit, quandoFalha: (erro: String?) -> Unit) {
+        repository.buscaTodos(quandoSucesso, quandoFalha)
     }
 
 }
